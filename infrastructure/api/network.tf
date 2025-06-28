20e60bd0b9961151f4067f555531372824b2da22
Om Mishra <32200996+mishraomp@users.noreply.github.com>
2025-06-27 19:02:59 -0700

    chore: aws initial pipeline setup bash script (#28)

--- infrastructure/api/network.tf
+++ infrastructure/api/network.tf
@@ -7,17 +7,14 @@ locals {
     unclass = "UnClass"
   }
   environment        = local.env_map[lower(var.target_env)]
-  vpc_name           = "${local.environment}_vpc"
-  availability_zones = ["a", "b"]
-  web_subnet_names   = [for az in local.availability_zones : "Web_${local.environment}_az${az}_net"]
-  app_subnet_names   = [for az in local.availability_zones : "App_${local.environment}_az${az}_net"]
-  data_subnet_names  = [for az in local.availability_zones : "Data_${local.environment}_az${az}_net"]
-
-  security_group_name_suffix = "_sg"
-
-  web_security_group_name  = "Web${local.security_group_name_suffix}"
-  app_security_group_name  = "App${local.security_group_name_suffix}"
-  data_security_group_name = "Data${local.security_group_name_suffix}"
+  vpc_name           = "${local.environment}"
+  availability_zones = ["A", "B"]
+  web_subnet_names   = [for az in local.availability_zones : "${local.environment}-Web-MainTgwAttach-${az}"]
+  app_subnet_names   = [for az in local.availability_zones : "${local.environment}-App-${az}"]
+  data_subnet_names  = [for az in local.availability_zones : "${local.environment}-Data-${az}"]
+  web_security_group_name  = "Web"
+  app_security_group_name  = "App"
+  data_security_group_name = "Data"
 }
 
 data "aws_vpc" "main" {
@@ -90,3 +87,4 @@ data "aws_security_group" "app" {
 data "aws_security_group" "data" {
   name = local.data_security_group_name
 }
+
