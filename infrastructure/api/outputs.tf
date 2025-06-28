4b8dcf773f2af8b2ab06b3642e868dac989ab517
OMPRAKASH MISHRA <omprakashmishra3978@gmail.com>
2025-06-27 23:29:54 -0700

    fix: cf config

--- infrastructure/api/outputs.tf
+++ infrastructure/api/outputs.tf
@@ -1,8 +1,8 @@
 output "apigw_url" {
   description = "Base URL to call the API (CloudFront if public, direct API Gateway if private)"
-  value       = var.is_public_api ? module.cloudfront_api[0].distribution_url : module.api_gateway.api_endpoint
+  value       = var.is_public_api ? module.cloudfront_api[0].distribution_url : null
 }
-
+/* 
 output "api_gateway_id" {
   description = "API Gateway ID"
   value       = module.api_gateway.api_id
@@ -12,7 +12,7 @@ output "api_gateway_endpoint" {
   description = "API Gateway endpoint URL"
   value       = module.api_gateway.api_endpoint
 }
-
+ */
 output "cloudfront_distribution_id" {
   description = "CloudFront distribution ID (if public API)"
   value       = var.is_public_api ? module.cloudfront_api[0].distribution_id : null
